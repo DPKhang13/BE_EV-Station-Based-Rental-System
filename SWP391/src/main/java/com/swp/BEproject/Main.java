c00059a9115ee1d3683696d560215d08dfa7eb63
Thai Nhut Hoang <thainhuthoang.super@gmail.com>
2025-09-19 16:40:51 +0700

    Test 3

--- SWP391/src/main/java/com/swp/BEproject/Main.java
+++ SWP391/src/main/java/com/swp/BEproject/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("Khangngu");
             System.out.println("khangnguvcl");
+            System.out.printf("123");
         }
     }
 }
