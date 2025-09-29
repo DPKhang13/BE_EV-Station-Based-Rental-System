8329c04a9d644b06911ba4d5ace3cfc8316cb841
En Ci Ây <canhan7325@gmail.com>
2025-09-29 18:40:28 +0700

    FinalFileProject

---
+++ projectswp391/src/main/java/com/group6/projectswp391/Projectswp391Application.java
@@ -0,0 +1,13 @@
+package com.group6.projectswp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectswp391Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projectswp391Application.class, args);
+    }
+
+}
