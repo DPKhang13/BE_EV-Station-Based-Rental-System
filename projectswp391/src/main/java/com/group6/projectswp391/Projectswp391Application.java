093751bac888f679f74c3551a3aee4df6c3b74d0
Đặng Phúc Khang <dangphuckhang10b2@gmail.com>
2025-09-29 23:31:45 +0700

    Code lai

--- projectswp391/src/main/java/com/group6/projectswp391/Projectswp391Application.java
+++ projectswp391/src/main/java/com/group6/projectswp391/Projectswp391Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Projectswp391Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Projectswp391Application.class, args);
+        System.out.println("Hello");
     }
 
 }
