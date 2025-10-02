4ec83818f6d1e6939f6db3ab3c8cb9e972f7b97f
En Ci Ây <canhan7325@gmail.com>
2025-10-02 13:58:26 +0700

    Cookies

---
+++ projectswp391/src/main/java/com/group6/Rental_Car/entities/User.java
@@ -0,0 +1,41 @@
+package com.group6.Rental_Car.entities;
+
+import com.group6.Rental_Car.enums.Role;
+import com.group6.Rental_Car.enums.UserStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "[User]") // 'User' là keyword trong SQL Server nên để trong []
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID userId;
+
+    private String fullName;
+
+    private String password;
+
+    private String phone;
+    @NotNull
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private String kycStatus;
+}
