503c78cc459cc517f98eb62818a2768ba6cf3623
Đặng Phúc Khang <dangphuckhang10b2@gmail.com>
2025-10-10 00:53:38 +0700

    Validate Vehicle Attribute

---
+++ projectswp391/src/main/java/com/group6/Rental_Car/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.group6.Rental_Car.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
--- projectswp391/src/main/java/com/group6/Rental_Car/services/Jwt/MailService.java
+++
@@ -1,6 +0,0 @@
-package com.group6.Rental_Car.services.Jwt;
-
-public interface MailService {
-    void sendOtp(String toEmail, String otp);
-}
-
