f493edcfe31c7f6522d82e33165904ba8ca76817
Đặng Phúc Khang <dangphuckhang10b2@gmail.com>
2025-11-26 22:24:53 +0700

    Update Photos

--- projectswp391/src/main/java/com/group6/Rental_Car/entities/Photo.java
+++ projectswp391/src/main/java/com/group6/Rental_Car/entities/Photo.java
@@ -1,5 +1,6 @@
 package com.group6.Rental_Car.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -19,6 +20,7 @@ public class Photo {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private User user;
 
     @Column(name = "photo_url", nullable = false, columnDefinition = "text")
